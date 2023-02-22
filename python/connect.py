e7d24bba5831371b00f1ee1ea3c5181ea3b30bfe
drbeefsupreme <jon@tlon.io>
2023-02-21 20:04:43 -0500

    python: try to connect via rust

---
+++ python/connect.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+#RPC library
+from simple_rpc import Interface
+#web app library
+#from flask import Flask, request
+import time, os
+#from config import Config
+
+VERBOSE = True
+SERIAL_PORT = '/dev/ttyACM0'
+READ_TIMEOUT = 5
+
+#app = Flask(__name__)
+#app.config.from_object(Config)
+interface = Interface(SERIAL_PORT)
+#import processing
+
+
+if __name__ == '__main__':
+    options = {
+        'serial_port': SERIAL_PORT,
+        'read_timeout': READ_TIMEOUT,
+        'verbose': VERBOSE
+    }
+
+
+    if interface is None:
+        raise Exception('[-] connect_to_serial_port failed')
+
+    print("connection successful")
+
+
+    time.sleep(1)
+
+    app.run(host='0.0.0.0', port=80, debug=True)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+//use notcurses::*;
+use pyo3::prelude::*;
+//use pyo3::types::PyTuple;
+
+fn main() -> PyResult<()> {
+    let py_connect = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/python/connect.py"));
+    let from_python = Python::with_gil(|py| -> PyResult<Py<PyAny>> {
+//        let simple_rpc = PyModule::import(py, "arduino-simple-rpc").expect("bweh");
+        let connect: Py<PyAny> = PyModule::from_code(py, py_connect, "", "")?
+            .getattr("run")?
+            .into();
+        connect.call0(py)
+    });
+
+    println!("py: {}", from_python?);
+    Ok(())
 }
