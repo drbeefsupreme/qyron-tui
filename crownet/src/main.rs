f22bfa01981ead3ca1e38b41237620b4d8c00ca2
drbeefsupreme <drbeefsupreme@discordja.net>
2023-09-30 09:35:20 +0100

    enum for selection

--- crownet/src/main.rs
+++ crownet/src/main.rs
@@ -9,6 +9,164 @@ use crownet::Command;
 
 static mut bweh: bool = true;
 
+fn main() -> NcResult<()> {
+    let rpc_config = py_rpc::Config::new();
+    let a = py_rpc::connect(&rpc_config);
+
+    println!("connect: {:?}", a);
+
+    let nc: &mut Nc = unsafe { Nc::new()? };
+    let stdplane: &mut NcPlane = unsafe { nc.stdplane() };
+    stdplane.set_fg_rgb(0x40f040);
+
+    let planes = Planes::init(stdplane);
+
+    let mut current_plane = CurrentPlane::TextBox;
+
+    loop {
+        match current_plane {
+            CurrentPlane::TextBox => {
+                text_box(nc, &rpc_config, stdplane, planes.reader, &mut current_plane);
+            },
+            CurrentPlane::Selector => {
+                run_selector(nc, &rpc_config, stdplane, planes.selector, &mut current_plane);
+            }
+        }
+    }
+
+    unsafe { nc.stop()? };
+
+    Ok(())
+}
+
+//TODO make a selector for which layer to write on
+fn text_box(nc: &mut Nc, rpc_config: &py_rpc::Config, stdplane: &mut NcPlane, reader: &mut NcReader,
+    current_plane: &mut CurrentPlane) {
+    let mut ni: NcInput = NcInput::new_empty();
+    unsafe { ncreader_clear(reader) };
+
+    loop {
+        match current_plane {
+            CurrentPlane::TextBox => {
+                let keypress: NcReceived = nc.get_blocking(Some(&mut ni)).expect("keypress");
+                match keypress {
+                   NcReceived::Char(ch) => {
+                       match ch {
+                           a => {
+                               let mut vec = Vec::<u8>::new();
+                               vec.push(a as u8);
+                               let print_this = unsafe { CString::from_vec_unchecked(vec) };
+                               unsafe {
+                                   ncreader_write_egc(reader as *mut ncreader, print_this.as_ptr() as *const u8);
+                               }
+                           }
+                       }
+                   },
+                   NcReceived::Key(ev) => match ev {
+                       NcKey::Enter => {
+                           let contents = unsafe { ncreader_contents(reader as *mut ncreader) };
+                           let cstr_contents = unsafe { CStr::from_ptr(contents) };
+                           let contents_string = cstr_contents.to_str().unwrap().to_owned();
+                           send_text(&rpc_config, contents_string);
+                       },
+                       NcKey::Home => {
+                           *current_plane = CurrentPlane::Selector;
+                           break;
+                       },
+                       _ => break,
+                   },
+                _ => (),
+               }
+            },
+            _ => break,
+        }
+        nc.render();
+    }
+}
+
+fn send_text(rpc_config: &py_rpc::Config, text: String) {
+    py_rpc::text(&rpc_config, text);
+}
+
+fn send_choice(choice: Command, rpc_config: &py_rpc::Config, nc: &mut Nc) {
+    match choice {
+        Command::Clear => py_rpc::clear(&rpc_config),
+        Command::Caw => py_rpc::caw(&rpc_config),
+        Command::Dopamine => py_rpc::dopamine(&rpc_config),
+        Command::RandomPixels => py_rpc::pixels(&rpc_config),
+        Command::RandomShapes => py_rpc::shapes(&rpc_config),
+        Command::Temperature => py_rpc::temp(&rpc_config),
+        Command::NextGif     => py_rpc::nextGif(&rpc_config),
+        Command::NoGif       => py_rpc::noGif(&rpc_config),
+        Command::RandomShapesBG => py_rpc::shapesBg(&rpc_config),
+        Command::RandomPixelsBG => py_rpc::pixelsBg(&rpc_config),
+        Command::Text => Ok(()),
+        _ => Ok(()),
+    };
+}
+
+fn run_selector(nc: &mut Nc, rpc_config: &py_rpc::Config, stdplane: &mut NcPlane, selector: &mut NcSelector,
+    current_plane: &mut CurrentPlane) {
+    let mut ni: NcInput = NcInput::new_empty();
+
+    // do not wait for input before first rendering
+    nc.render();
+
+    loop {
+        match current_plane {
+            CurrentPlane::Selector => {
+                let keypress: NcReceived = nc.get_blocking(Some(&mut ni)).unwrap();
+
+                if !selector.offer_input(ni) {
+                    // do not consider release key: only press
+                    if ni.evtype == NcInputType::Release as u32 {
+                        continue;
+                    }
+
+                    match keypress {
+                        NcReceived::Char(ch) => {
+                            match ch {
+                                // Q => quit
+                                'q' | 'Q' => {
+                                    send_choice(Command::Quit, &rpc_config, nc);
+                                },
+                                // S => down
+                                's' | 'S' => {
+                                    selector.nextitem().unwrap();
+                                },
+                                // W => up
+                                'w' | 'W' => {
+                                    selector.previtem().unwrap();
+                                },
+                                _ => (),
+                            }
+                        },
+                        NcReceived::Key(ev) => match ev {
+                            NcKey::Enter => {
+                                let choice = selector.selected().ok_or_else(|| NcError::new()).unwrap();
+                                send_choice(Command::from_str(&choice), &rpc_config, nc);
+                            },
+                            NcKey::Home => {
+                                *current_plane = CurrentPlane::TextBox;
+                            },
+                            _ => break,
+                        },
+                        _ => break,
+                    }
+                 }
+            },
+            _ => break,
+        }
+
+        nc.render();
+    }
+}
+
+enum CurrentPlane {
+    Selector,
+    TextBox,
+}
+
 struct Planes<'a> {
     reader: &'a mut NcReader,
     selector: &'a mut NcSelector,
@@ -18,6 +176,7 @@ impl Planes<'_> {
     pub fn init(stdplane: &mut NcPlane) -> Self {
         let reader_opts: NcPlaneOptions = NcPlaneOptions::new_aligned(10, NcAlign::Center, 150, 80);
         let reader_plane: &mut NcPlane = NcPlane::new_child(stdplane, &reader_opts).unwrap();
+        reader_plane.set_bg_rgb(0x40f040); //TODO is this doing anything?
         let mut reader = NcReader::new(reader_plane).unwrap();
 
         let plane_opts: NcPlaneOptions = NcPlaneOptions::new_aligned(15, NcAlign::Left, 15, 80);
@@ -51,147 +210,5 @@ impl Planes<'_> {
             reader,
             selector,
         }
-    }
-}
-
-fn main() -> NcResult<()> {
-    let rpc_config = py_rpc::Config::new();
-    let a = py_rpc::connect(&rpc_config);
-
-    println!("connect: {:?}", a);
-
-    let nc: &mut Nc = unsafe { Nc::new()? };
-    let stdplane: &mut NcPlane = unsafe { nc.stdplane() };
-    stdplane.set_fg_rgb(0x40f040);
-
-    let planes = Planes::init(stdplane);
-
-    loop {
-        if unsafe { bweh } {
-            text_box(nc, &rpc_config, stdplane, planes.reader);
-        } else {
-            run_selector(nc, &rpc_config, stdplane, planes.selector);
-        }
-    }
-
-    unsafe { nc.stop()? };
-
-    Ok(())
-}
-
-fn text_box(nc: &mut Nc, rpc_config: &py_rpc::Config, stdplane: &mut NcPlane, reader: &mut NcReader) {
-    let mut ni: NcInput = NcInput::new_empty();
-    unsafe { ncreader_clear(reader) };
-
-    loop {
-        if unsafe { bweh } {
-            let keypress: NcReceived = nc.get_blocking(Some(&mut ni)).expect("keypress");
-            match keypress {
-               NcReceived::Char(ch) => {
-                   match ch {
-                       a => {
-                           let mut vec = Vec::<u8>::new();
-                           vec.push(a as u8);
-                           let print_this = unsafe { CString::from_vec_unchecked(vec) };
-                           unsafe {
-                               ncreader_write_egc(reader as *mut ncreader, print_this.as_ptr() as *const u8);
-                           }
-                       }
-                   }
-               },
-               NcReceived::Key(ev) => match ev {
-                   NcKey::Enter => {
-                       let contents = unsafe { ncreader_contents(reader as *mut ncreader) };
-                       let cstr_contents = unsafe { CStr::from_ptr(contents) };
-                       let contents_string = cstr_contents.to_str().unwrap().to_owned();
-                       send_text(&rpc_config, contents_string);
-                   },
-                   NcKey::Home => {
-                       unsafe { bweh = !bweh }
-                       break;
-                   },
-                   _ => break,
-               },
-            _ => (),
-           }
-        }  else {
-            break;
-        }
-        nc.render();
-    }
-}
-
-fn send_text(rpc_config: &py_rpc::Config, text: String) {
-    py_rpc::text(&rpc_config, text);
-}
-
-fn send_choice(choice: Command, rpc_config: &py_rpc::Config, nc: &mut Nc) {
-    match choice {
-        Command::Clear => py_rpc::clear(&rpc_config),
-        Command::Caw => py_rpc::caw(&rpc_config),
-        Command::Dopamine => py_rpc::dopamine(&rpc_config),
-        Command::RandomPixels => py_rpc::pixels(&rpc_config),
-        Command::RandomShapes => py_rpc::shapes(&rpc_config),
-        Command::Temperature => py_rpc::temp(&rpc_config),
-        Command::NextGif     => py_rpc::nextGif(&rpc_config),
-        Command::NoGif       => py_rpc::noGif(&rpc_config),
-        Command::RandomShapesBG => py_rpc::shapesBg(&rpc_config),
-        Command::RandomPixelsBG => py_rpc::pixelsBg(&rpc_config),
-        Command::Text => Ok(()),
-        _ => Ok(()),
-    };
-}
-
-fn run_selector(nc: &mut Nc, rpc_config: &py_rpc::Config, stdplane: &mut NcPlane, selector: &mut NcSelector) {
-    let mut ni: NcInput = NcInput::new_empty();
-
-    // do not wait for input before first rendering
-    nc.render();
-
-    loop {
-        if unsafe { bweh } {
-            break;
-        }
-        let keypress: NcReceived = nc.get_blocking(Some(&mut ni)).unwrap();
-
-        if !selector.offer_input(ni) {
-            // do not consider release key: only press
-            if ni.evtype == NcInputType::Release as u32 {
-                continue;
-            }
-
-            match keypress {
-                NcReceived::Char(ch) => {
-                    match ch {
-                        // Q => quit
-                        'q' | 'Q' => {
-                            send_choice(Command::Quit, &rpc_config, nc);
-                        },
-                        // S => down
-                        's' | 'S' => {
-                            selector.nextitem().unwrap();
-                        },
-                        // W => up
-                        'w' | 'W' => {
-                            selector.previtem().unwrap();
-                        },
-                        _ => (),
-                    }
-                },
-                NcReceived::Key(ev) => match ev {
-                    NcKey::Enter => {
-                        let choice = selector.selected().ok_or_else(|| NcError::new()).unwrap();
-                        send_choice(Command::from_str(&choice), &rpc_config, nc);
-                    },
-                    NcKey::Home => {
-                        unsafe { bweh = !bweh };
-                    },
-                    _ => break,
-                },
-                _ => break,
-            }
-        }
-
-        nc.render();
     }
 }
