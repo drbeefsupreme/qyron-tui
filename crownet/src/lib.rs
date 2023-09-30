508200086c575f92751f033fbfd052c2263ec5ad
drbeefsupreme <drbeefsupreme@discordja.net>
2023-09-30 09:44:33 +0100

    more refactor

--- crownet/src/lib.rs
+++ crownet/src/lib.rs
@@ -1,3 +1,12 @@
+pub mod text_box;
+
+use py_rpc;
+//use notcurses::*;
+use notcurses::sys::{widgets::*, *};
+use libnotcurses_sys::c_api::{ncreader, ncreader_write_egc, ncreader_contents, ncreader_destroy,
+    notcurses_drop_planes, ncreader_clear};
+use std::ffi::{CStr, CString};
+
 //  commands to send to LED matrix
 pub enum Command {
     Clear,
@@ -29,6 +38,57 @@ impl Command {
             "Random shapes BG" => Command::RandomShapesBG,
             "Random pixels BG" => Command::RandomShapesBG,
             _               => Command::Invalid,
+        }
+    }
+}
+
+pub enum CurrentPlane {
+    Selector,
+    TextBox,
+}
+
+pub struct Planes<'a> {
+    pub reader: &'a mut NcReader,
+    pub selector: &'a mut NcSelector,
+}
+
+impl Planes<'_> {
+    pub fn init(stdplane: &mut NcPlane) -> Self {
+        let reader_opts: NcPlaneOptions = NcPlaneOptions::new_aligned(10, NcAlign::Center, 150, 80);
+        let reader_plane: &mut NcPlane = NcPlane::new_child(stdplane, &reader_opts).unwrap();
+        reader_plane.set_bg_rgb(0x40f040); //TODO is this doing anything?
+        let mut reader = NcReader::new(reader_plane).unwrap();
+
+        let plane_opts: NcPlaneOptions = NcPlaneOptions::new_aligned(15, NcAlign::Left, 15, 80);
+        let sel_plane: &mut NcPlane = NcPlane::new_child(stdplane, &plane_opts).unwrap();
+        let mut selector = NcSelector::builder()
+            .item("Clear", "clears the chyron")
+            .item("CAW", "CAW CAW CAW CAW CAW")
+            .item("DOPAMINE", "DOPAMINE DOPAMINE DOPAMINE DOPAMINE DOPAMINE")
+            .item("Random pixels", "asdfasdhfkalshdg")
+            .item("Random pixels BG", "skrrt")
+            .item("Random shapes", "yes")
+            .item("Random shapes BG", "YES")
+            .item("Next gif", "bweh")
+            .item("No gif", "bye")
+    //        .item("Temperature", "DEBUG: CPU temp")
+            .title("CrowNet")
+            .secondary("Institute for Advanced Levels")
+            .footer("The Too Late Show with Dr. Beelzebub Crow")
+            .max_display(4)
+            .default_item(1)
+            .box_channels(NcChannels::from_rgb(0x20e040, 0x202020))
+            .item_channels(
+                NcChannels::from_rgb(0xe08040, 0),
+                NcChannels::from_rgb(0x80e040, 0),
+            )
+            .secondary_channels(NcChannels::from_rgb(0xe00040, 0x200000))
+            .title_channels(NcChannels::from_rgb(0xffff80, 0x000020))
+            .finish(sel_plane).unwrap();
+
+        Self {
+            reader,
+            selector,
         }
     }
 }
--- crownet/src/main.rs
+++ crownet/src/main.rs
@@ -5,9 +5,9 @@ use libnotcurses_sys::c_api::{ncreader, ncreader_write_egc, ncreader_contents, n
     notcurses_drop_planes, ncreader_clear};
 use std::ffi::{CStr, CString};
 
-use crownet::Command;
+use crownet::{Command, Planes, CurrentPlane};
 
-static mut bweh: bool = true;
+use crownet::text_box::*;
 
 fn main() -> NcResult<()> {
     let rpc_config = py_rpc::Config::new();
@@ -37,55 +37,6 @@ fn main() -> NcResult<()> {
     unsafe { nc.stop()? };
 
     Ok(())
-}
-
-//TODO make a selector for which layer to write on
-fn text_box(nc: &mut Nc, rpc_config: &py_rpc::Config, stdplane: &mut NcPlane, reader: &mut NcReader,
-    current_plane: &mut CurrentPlane) {
-    let mut ni: NcInput = NcInput::new_empty();
-    unsafe { ncreader_clear(reader) };
-
-    loop {
-        match current_plane {
-            CurrentPlane::TextBox => {
-                let keypress: NcReceived = nc.get_blocking(Some(&mut ni)).expect("keypress");
-                match keypress {
-                   NcReceived::Char(ch) => {
-                       match ch {
-                           a => {
-                               let mut vec = Vec::<u8>::new();
-                               vec.push(a as u8);
-                               let print_this = unsafe { CString::from_vec_unchecked(vec) };
-                               unsafe {
-                                   ncreader_write_egc(reader as *mut ncreader, print_this.as_ptr() as *const u8);
-                               }
-                           }
-                       }
-                   },
-                   NcReceived::Key(ev) => match ev {
-                       NcKey::Enter => {
-                           let contents = unsafe { ncreader_contents(reader as *mut ncreader) };
-                           let cstr_contents = unsafe { CStr::from_ptr(contents) };
-                           let contents_string = cstr_contents.to_str().unwrap().to_owned();
-                           send_text(&rpc_config, contents_string);
-                       },
-                       NcKey::Home => {
-                           *current_plane = CurrentPlane::Selector;
-                           break;
-                       },
-                       _ => break,
-                   },
-                _ => (),
-               }
-            },
-            _ => break,
-        }
-        nc.render();
-    }
-}
-
-fn send_text(rpc_config: &py_rpc::Config, text: String) {
-    py_rpc::text(&rpc_config, text);
 }
 
 fn send_choice(choice: Command, rpc_config: &py_rpc::Config, nc: &mut Nc) {
@@ -162,53 +113,3 @@ fn run_selector(nc: &mut Nc, rpc_config: &py_rpc::Config, stdplane: &mut NcPlane
     }
 }
 
-enum CurrentPlane {
-    Selector,
-    TextBox,
-}
-
-struct Planes<'a> {
-    reader: &'a mut NcReader,
-    selector: &'a mut NcSelector,
-}
-
-impl Planes<'_> {
-    pub fn init(stdplane: &mut NcPlane) -> Self {
-        let reader_opts: NcPlaneOptions = NcPlaneOptions::new_aligned(10, NcAlign::Center, 150, 80);
-        let reader_plane: &mut NcPlane = NcPlane::new_child(stdplane, &reader_opts).unwrap();
-        reader_plane.set_bg_rgb(0x40f040); //TODO is this doing anything?
-        let mut reader = NcReader::new(reader_plane).unwrap();
-
-        let plane_opts: NcPlaneOptions = NcPlaneOptions::new_aligned(15, NcAlign::Left, 15, 80);
-        let sel_plane: &mut NcPlane = NcPlane::new_child(stdplane, &plane_opts).unwrap();
-        let mut selector = NcSelector::builder()
-            .item("Clear", "clears the chyron")
-            .item("CAW", "CAW CAW CAW CAW CAW")
-            .item("DOPAMINE", "DOPAMINE DOPAMINE DOPAMINE DOPAMINE DOPAMINE")
-            .item("Random pixels", "asdfasdhfkalshdg")
-            .item("Random pixels BG", "skrrt")
-            .item("Random shapes", "yes")
-            .item("Random shapes BG", "YES")
-            .item("Next gif", "bweh")
-            .item("No gif", "bye")
-    //        .item("Temperature", "DEBUG: CPU temp")
-            .title("CrowNet")
-            .secondary("Institute for Advanced Levels")
-            .footer("The Too Late Show with Dr. Beelzebub Crow")
-            .max_display(4)
-            .default_item(1)
-            .box_channels(NcChannels::from_rgb(0x20e040, 0x202020))
-            .item_channels(
-                NcChannels::from_rgb(0xe08040, 0),
-                NcChannels::from_rgb(0x80e040, 0),
-            )
-            .secondary_channels(NcChannels::from_rgb(0xe00040, 0x200000))
-            .title_channels(NcChannels::from_rgb(0xffff80, 0x000020))
-            .finish(sel_plane).unwrap();
-
-        Self {
-            reader,
-            selector,
-        }
-    }
-}
